034d37d10a4cd72986e3d2cabc09fc9e916d1eb3
Gabriele <gabrielebonipistolesi@gmail.com>
2026-09-09 14:27:49 +0200

    add example

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 # python3 wayland/client/generator.py wayland/client/protocols
 # odin run src/basic -collection:wayland=wayland/
-odin run src/vulkan -collection:wayland=wayland/
+# odin run src/vulkan -collection:wayland=wayland/
+odin run src/vulkan+input -collection:wayland=wayland/
